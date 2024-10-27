e22fe67e56b8595b5e06acae47f47ef6d515d933
tumalishakya <temb5020@uni.sydney.edu.au>
2024-10-28 01:56:49 +1100

    updated teleop functionality

--- src/explore/include/explore/teleop.hpp
+++ src/explore/include/explore/teleop.hpp
@@ -10,20 +10,23 @@ Written by Tumali Embuldeniya, 2024
 #ifndef TELEOP_H
 #define TELEOP_H
 
-
 #include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
 #include <iostream>
+#include <string>
+#include <geometry_msgs/msg/twist.hpp>
+#include <chrono>
+#include <memory>
 
-class Teleop : public rclcpp::Node 
-{
+
+class Teleop : public rclcpp::Node {
     public:
         Teleop();
 
     private:
         void teleop_run();
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr teleop_pub_;
-}
+        rclcpp::TimerBase::SharedPtr timer_;
+};
 
 
 #endif
--- src/explore/src/teleop.cpp
+++ src/explore/src/teleop.cpp
@@ -6,7 +6,7 @@ This file contains the implementation of the  teleop class
 Written by Tumali Embuldeniya, 2024
 */
 
-#include "teleop.hpp"
+#include "explore/teleop.hpp"
 
 
 Teleop::Teleop() : Node("teleop") 
@@ -14,48 +14,55 @@ Teleop::Teleop() : Node("teleop")
     teleop_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
     RCLCPP_INFO(this->get_logger(), "Teleop Node Started. Use WASD to control the robot.");
 
-    teleop_run();
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(100), 
+                                     std::bind(&Teleop::teleop_run, this));
 }
 
 
 void Teleop::teleop_run()
 {
-    char user_input_;
+    std::string user_input_;
     geometry_msgs::msg::Twist vel;
 
     std::cout << "Enter input (WASD - to drive, X - to quit): ";
     std::cin >> user_input_;
 
-    while (user_input_ != "x")
+    if (user_input_ == "x") {
+        rclcpp::shutdown(); // Exit if "x" is entered
+        return;
+    }
+
+    if (user_input_.compare("w") == 0)
+    {
+        vel.linear.x = 0.2;
+    }
+    else if(user_input_.compare("s") == 0)
+    {
+        vel.linear.x = -0.2;
+    }
+    else if(user_input_.compare("a") == 0)
+    {
+        vel.angular.z = 0.2;
+    }
+    else if(user_input_.compare("d") == 0)
+    {
+        vel.angular.z = -0.2;
+    }
+    else if(user_input_.compare("q") == 0)
     {
         vel.linear.x = 0.0;
         vel.angular.z = 0.0;
-
-        switch(user_input_)
-        {
-            case 'w':
-                vel.linear.x = 0.5;
-                break;
-            case 's';
-                vel.linear.x = -0.5;
-                break;
-            case 'a';
-                vel.angular.z = 0.5;
-                break;
-            case 'd';
-                vel.angular.z = -0.5;
-                break;
-            default:
-                RCLCPP_WARN(this->get_logger(), "Unknown command! (Use 'WASD' to drive, 'X' to quit).");
-                continue;
-        }
-
-        teleop_pub_->publish(vel);
-
-        std::cout << "Enter input (WASD - to drive, X - to quit): ";
-        std::cin >> user_input_;
     }
+    else
+    {
+        vel.linear.x = 0.0;
+        vel.angular.z = 0.0;
+        RCLCPP_WARN(this->get_logger(), "Unknown command! (Use 'WASD' to drive, 'X' to quit).");
+    }
+
+    teleop_pub_->publish(vel);
 }
+
 
 
 
